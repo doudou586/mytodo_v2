9ac3afe0cfe0ca86c834a7d3fa931274a736adfd
Daojing. Zhou <doudou586@gmail.com>
2019-08-15 14:16:05 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="app">
+		<h2>My Todo App!</h2>
+		<TodoList/>
+	</div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+	components: {
+		TodoList
+	}
+}
+</script>
+
---
+++ components/TodoListItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    {{ todo.text  }}__({{todo.id}})
+    <button @click="$emit('remove', todo.id)">
+      -
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
